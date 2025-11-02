f620fa5b22c974047b154f9871f3650401dfb9ee
rubenaguilardev <ragui56@wgu.edu>
2025-11-01 19:12:19 -0700

    add star background

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -30,7 +30,7 @@ const ThemeToggle = () => {
 
     return (
         <button onClick={toggleTheme} className={cn('fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300 forcus:outline-hidden')}
-        >{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/> : <Moon className='h-6 w-6 text-blue-900'/>}</button>
+        >{isDarkMode ? <Sun className='h-8 w-8 text-yellow-300'/> : <Moon className='h-8 w-8 text-blue-900'/>}</button>
     )
 }
 
