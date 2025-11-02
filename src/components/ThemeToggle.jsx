d935172ccfc119f4068c0cc74884f27bd730514a
rubenaguilardev <ragui56@wgu.edu>
2025-11-01 18:06:06 -0700

    add dark/light toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,8 +4,19 @@ import { useState } from 'react'
 const ThemeToggle = () => {
     const [isDarkMode, setIsDarkMode] = useState(false)
 
+    const toggleTheme = () => {
+        if (isDarkMode) {
+            document.documentElement.classList.remove('dark')
+            setIsDarkMode(false)
+        } else {
+            document.documentElement.classList.add('dark')
+            setIsDarkMode(true)
+        }
+    }
+
     return (
-        <button>{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/> : <Moon className='h-6 w-6 text-blue-900'/>}</button>
+        <button onClick={toggleTheme}
+        >{isDarkMode ? <Sun className='h-6 w-6 text-yellow-300'/> : <Moon className='h-6 w-6 text-blue-900'/>}</button>
     )
 }
 
