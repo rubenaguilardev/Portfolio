78c442f09a570a94e03a0463ba9ef15a29cf056f
rubenaguilardev <ragui56@wgu.edu>
2025-11-01 17:59:34 -0700

    add theme toggle component, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import { Home } from "lucide-react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import NotFound from "./pages/NotFound"
 
 const App = () => {
   return (
